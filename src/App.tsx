2f2569621fe70ae645ddb62a9d2e302385b77511
itumandal <itumandal22@gmail.com>
2025-09-10 15:47:20 +0530

    updated readme

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import "./App.css";
 
 function App() {
-  return <div className="p-4 flex bg-amber-200">Hello</div>;
+  return <div className="p-4 flex bg-amber-200">Hello Project</div>;
 }
 
 export default App;
